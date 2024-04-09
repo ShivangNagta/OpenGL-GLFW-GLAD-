7c5b404058a98e4e608a2ec7e5307e5c272aab5b
Shivang Nagta <141389245+ShivangNagta@users.noreply.github.com>
2024-04-09 23:04:12 +0530

    Organized

--- YTOpenGL/ShaderClass.h
+++ YTOpenGL/ShaderClass.h
@@ -1,1 +1,24 @@
-#pragma once
+#ifndef SHADER_CLASS_H
+#define SHADER_CLASS_H
+using namespace std;
+
+#include<glad/glad.h>
+#include<fstream>
+#include<string>
+#include<sstream>
+#include<iostream>
+#include<cerrno>
+
+string get_file_contents(const char* filename);
+
+class Shader {
+public:
+	GLuint ID;
+	Shader(const char* vertexFile, const char* fragmentFile);
+
+	void Activate();
+	void Delete();
+};
+
+
+#endif 
